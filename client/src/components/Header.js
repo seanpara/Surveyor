9052521a751f8af73c9b1e73d4658a11e2e999e0
seanpara <spara77@gmail.com>
2019-02-27 16:42:04 -0500

    fleshed out React app and express routes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
   renderContent() {
@@ -15,7 +16,7 @@ class Header extends Component {
       default:
         return (
           <li>
-            <a>Log Out</a>
+            <a href="/api/logout">Log Out</a>
           </li>
         );
     }
@@ -25,9 +26,12 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo" href="">
+          <Link
+            to={this.props.auth ? "/surveys" : "/"}
+            className="left brand-logo"
+          >
             Surveyor
-          </a>
+          </Link>
           <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
