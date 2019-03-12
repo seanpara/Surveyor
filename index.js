230e0bb5d6a69397dab52c41739991c96cbf52b5
seanpara <spara77@gmail.com>
2019-03-12 17:20:08 -0400

    added client deployment setup

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require("./services/passport");
 
 mongoose.connect(
   keys.mongoURI,
-  function(err) {
+  err => {
     if (err) {
       console.log("Error occurred while connecting to MongoDB Atlas...\n", err);
     }
@@ -32,6 +32,15 @@ app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
